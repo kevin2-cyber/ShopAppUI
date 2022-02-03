3c42952471499030fd1e0070640474466d381ef2
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-03 14:56:09 +0000

    created shop app ui starter project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_ui/screens/home/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Flutter Way',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "Gordita",
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
